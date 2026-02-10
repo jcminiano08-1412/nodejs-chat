b5e274fd90d766948b89eb473b9956d8ae473d97
jcmin1412 <jcminiano08@gmail.com>
2026-02-10 11:50:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: [
-            'http://localhost:3000',
-            'http://localhost:5500',
-            // 'https://your-frontend.com'
-        ],
+        origin: "*",
         methods: ['GET', 'POST'],
         credentials: true
     }
